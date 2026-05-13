5ee2442cdf0dbe38fcce9359fd25a692bf66a4d0
ItzValen01 <valentinomodaffari07@gmail.com>
2026-05-12 21:02:05 -0300

    Generar personaje aleatorio

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+let characters = []
+
 fetch("https://rickandmortyapi.com/api/character")
     .then(res => res.json())
     .then(data => {
         data.results.forEach(char => {
+            characters.push(char)
             document.getElementById("personajes").innerHTML += `
                 <div class="col-md-3 mb-4">
                     <div class="card">
@@ -21,3 +24,12 @@ fetch("https://rickandmortyapi.com/api/character")
             `
         })
     })
+
+function generar() {
+    const index = Math.floor(Math.random() * characters.length)
+    const char = characters[index];
+
+    document.getElementById("random-img").src = char.image
+    document.getElementById("random-title").innerText = char.name
+    document.getElementById("random-species").innerText = char.species
+}
